a1b907b6642d62241c3a19ace881c9d41983f2cc
Zulfiqor <theassassinaa@gmail.com>
2025-05-03 13:04:26 +0500

    first commit

--- RazorApp/Program.cs
+++ RazorApp/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddAutoMapper(typeof(InfrastructureProfile));
 builder.Services.AddScoped<IAuthorService, AuthorService>();
@@ -15,11 +14,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
